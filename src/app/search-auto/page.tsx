1edd0fd61a571ebaebfec1108549de2017f552eb
jahongirabduazimov707 <jahongirabduazimov707@gmail.com>
2025-08-06 05:45:15 +0500

    Updated

--- src/app/search-auto/page.tsx
+++ src/app/search-auto/page.tsx
@@ -1,13 +1,11 @@
-"use client"
-
-import React from "react";
+import React, { Suspense } from "react";
 import Filters from "@/sections/search-auto/Filters";
 
 const SearchAuto = () => {
   return (
-    <>
+    <Suspense fallback={<div className="text-center py-40">Загрузка...</div>}>
       <Filters />
-    </>
+    </Suspense>
   );
 };
 
